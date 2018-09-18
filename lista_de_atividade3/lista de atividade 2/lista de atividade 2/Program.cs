6822933cecf82752fa79aa6f2487469efcd04a64
Eric Almeida <37628478+almeieric@users.noreply.github.com>
2018-09-18 20:30:00 -0300

    primeiro git

---
+++ lista_de_atividade3/lista de atividade 2/lista de atividade 2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista_de_atividade_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ContaPoupanca poupador1 = new ContaPoupanca(2000);
+
+            Console.WriteLine(poupador1.CalcularJurosMensal(2000, 4));
+            Console.WriteLine("\n\n\nPressione qualquer tecla para sair...");
+
+            Console.ReadKey(true);
+
+
+        }//fim do Program
+    }
+    class ContaPoupanca
+    {
+
+
+        private double saldoPoupanca;
+        private double taxaDeJurosAnual;
+        
+
+        
+          
+        
+        public ContaPoupanca()
+        {
+            this.saldoPoupanca = 0.0;
+            this.taxaDeJurosAnual = 0.0;
+            
+
+        } 
+        public ContaPoupanca(double saldoPoupanca)
+        {
+            this.saldoPoupanca = saldoPoupanca;
+            
+        }
+        public double CalcularJurosMensal(double saldoPoupanca, double taxaDeJurosAnual)
+        {
+            return ((saldoPoupanca * taxaDeJurosAnual) /100 )/ 12;
+        }
+        static double AlterarTaxaJuros()
+        {
+            return 1;
+        }
+
+
+
+
+
+
+
+
+
+
+    }//fim do conta poupança
+}
